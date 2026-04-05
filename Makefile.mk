d94d967705630d63fef7c9b811909d24f7fabcd5
BRANDYR2D2 <brodg272@gmail.com>
2026-04-05 17:49:36 +1000

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -2,20 +2,10 @@ ifneq ($(RESDEV),1)
 include $(DEVKITPRO)/libnx/switch_rules
 endif
 
-# The name of your app
 TARGET := R2D2_Controller
-
-# Temporary build folder
 BUILD := build
+# Using a dot '.' tells the server 'Look right here in the main folder'
+SOURCES := .
+INCLUDES := .
 
-# This is the fix: It tells the server to look in the folder named 'source'
-# located in the same place as this Makefile.
-SOURCES := source
-
-# Standard folders
-INCLUDES := include
-DATA := data
-ICON := icon.jpg
-
-# The 'magic' line that does the work
 include $(DEVKITPRO)/libnx/switch_rules
